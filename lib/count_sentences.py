e0e76e1f2c7ad4ee7db17c76862176bb01870827
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-11-28 18:50:20 +0300

    completed

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
+import re
 class MyString:
-  pass
+    import re
+class MyString:
+  def __init__(self, value=""):
+      self._value = value
+      if type(self._value) != str:
+        print("The value must be a string.")
+      
+  def is_sentence(self):
+    return True if self._value.endswith(".") else False
+  def is_question(self):
+    return True if self._value.endswith("?") else False
+  def is_exclamation(self):
+    return True if self._value.endswith("!") else False
+  def count_sentences(self):
+    listy = re.sub(r'[!?]',".",self._value).split(".")
+    listy.remove("")
+    return len(listy)
+string = MyString()
+string.value= "1"
+# print(string.count_sentences())
+print(string.value)
